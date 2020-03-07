4334f7214543767a5761aebef1053a05d0547496
FreesiaBloom <agata.jasinska215@gmail.com>
2020-03-07 13:17:16 +0100

    creating vue cli project with bulma framework

---
+++ webportfolio/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="section" id="app">
+    <nav class="navbar" id="nav">
+      <div class="nav-item">
+        <router-link to="/">Home</router-link> |
+      </div>
+      <div class="nav-tem">
+        <router-link to="/contact">Contact</router-link>
+      </div>
+    </nav>
+    <router-view />
+  </section>
+</template>
+
+<style lang="scss">
+@import "~bulma/css/bulma.css";
+@import "./assets/main.scss";
+</style>
